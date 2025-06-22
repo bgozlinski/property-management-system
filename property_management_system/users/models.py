3b1ff37ff15493d2879e1546734489f9a8ac80bd
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2025-06-22 21:37:31 +0200

    Added Role assigment to Users users/models.py

--- property_management_system/users/models.py
+++ property_management_system/users/models.py
@@ -6,13 +6,24 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
+    class RoleChoices(models.IntegerChoices):
+        TENANT = 1,
+        LANDLORD = 2,
+        ADMINISTRATOR = 3
+
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    role = models.IntegerField(
+        choices=RoleChoices.choices,
+        default=RoleChoices.TENANT
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+
 
     def __str__(self):
         return self.email
