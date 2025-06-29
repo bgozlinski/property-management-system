e25c736668203380019efedd379e593909fa4532
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2025-06-29 02:40:06 +0200

    added logic and frontend for landlord checkbox

--- property_management_system/users/forms.py
+++ property_management_system/users/forms.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    is_landlord = forms.BooleanField(
+        required=False,
+        label="I want to register as a Landlord",
+        help_text="Check this box if you want to register as a Landlord"
+    )
 
     class Meta:
         model = CustomUser
--- property_management_system/users/views.py
+++ property_management_system/users/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CustomUserCreationForm
 from django.contrib.messages import get_messages
+from .models import CustomUser
 
-# Create your views here.
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('email')
-            messages.success(request, f'Account created for {user}!')
+            user = form.save(commit=False)  # Get the user object but don't save to DB yet
+            if form.cleaned_data.get('is_landlord'):
+                user.role = CustomUser.RoleChoices.LANDLORD
+            user.save()  # Now save the user with the updated role
+
+            email = form.cleaned_data.get('email')
+            messages.success(request, f'Account created for {email}!')
 
             storage = get_messages(request)
             for message in storage:
