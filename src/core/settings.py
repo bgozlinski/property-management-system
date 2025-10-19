1275a74fbb00a41c1bd4994365ee684245e5c0c0
bgozlinski <bartlomiej.gozlinski@gmail.com>
2025-10-19 12:46:18 +0200

    CI update

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from .env import env
+import sys
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,9 +138,13 @@ if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-CELERY_BROKER_URL = env("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
-CELERY_ACCEPT_CONTENT = ["application/json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = TIME_ZONE
+RUNNING_TESTS = (len(sys.argv) > 1 and sys.argv[1] == "test") or ("pytest" in " ".join(sys.argv))
+
+if RUNNING_TESTS:
+    CELERY_TASK_ALWAYS_EAGER = True
+    CELERY_TASK_EAGER_PROPAGATES = True
+    CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="memory://")
+    CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="cache+memory://")
+else:
+    CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default=CELERY_BROKER_URL)
